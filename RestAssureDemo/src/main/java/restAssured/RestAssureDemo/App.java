6fe9e710e4e9de4ad2bd583ed954572f8a3ef58f
kalyan7M <140342183+kalyan7M@users.noreply.github.com>
2024-05-29 16:23:14 +0530

    Add files via upload

---
+++ RestAssureDemo/src/main/java/restAssured/RestAssureDemo/App.java
@@ -0,0 +1,13 @@
+package restAssured.RestAssureDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
